fe521ff192561037986e72fc3be05cd3c92d5dd5
Gabriel Ferraz <gabrielmaiaferraz@gmail.com>
2018-03-12 17:15:05 +1300

    Added Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 
 import { AppComponent } from './app.component';
-import { WeaponComponent } from './weapon/weapon.component';
+import { AddWeaponComponent } from './weapon/add-weapon.component';
+import { ListWeaponsComponent } from './weapon/list-weapons/list-weapons.component';
+
+const appRoutes: Routes = [
+  { path: 'weapon', component: AddWeaponComponent },
+  // { path: 'weapon/:id',      component: HeroDetailComponent },
+  {
+    path: 'weapons',
+    component: ListWeaponsComponent,
+    data: { title: 'Weapons List' }
+  },
+  { path: '',
+    redirectTo: '/weapons',
+    pathMatch: 'full'
+  },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeaponComponent
+    AddWeaponComponent,
+    ListWeaponsComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
